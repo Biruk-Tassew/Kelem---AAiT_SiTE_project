3a7f7139d3792e8fa198c918093b2e2f69561a4a
Biruk-Tassew <biruktassew21@gmail.com>
2022-01-03 03:49:44 -0500

    initial commit

---
+++ src/main/java/com/kelem/kelem/KelemApplication.java
@@ -0,0 +1,13 @@
+package com.kelem.kelem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelem/kelem/KelemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelem.kelem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
