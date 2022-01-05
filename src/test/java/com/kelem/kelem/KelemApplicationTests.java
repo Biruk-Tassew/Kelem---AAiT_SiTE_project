be7af118b6e6750748f0e9929f247daee1208f96
hello-world-was-taken <haileformal@gmail.com>
2022-01-05 19:43:08 +0300

    add user model

--- src/test/java/com/kelem/kelem/KelemApplicationTests.java
+++ src/test/java/com/kelem/kelem/KelemApplicationTests.java
@@ -3,7 +3,7 @@ package com.kelem.kelem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=KelemApplicationTests.class)
 class KelemApplicationTests {
 
 	@Test
